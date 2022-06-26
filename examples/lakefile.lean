eb84c7fb47213b021347ffb3fe72d282b1f1f5ca
David Christiansen <david@davidchristiansen.dk>
2022-06-26 11:31:15 +0200

    Update Lean version

--- examples/lakefile.lean
+++ examples/lakefile.lean
@@ -4,3 +4,12 @@ open Lake DSL
 package examples {
   -- add configuration options here
 }
+
+lean_lib Examples {
+  -- add lib config here
+}
+
+@[defaultTarget]
+lean_exe examples {
+  root := `Main
+}
