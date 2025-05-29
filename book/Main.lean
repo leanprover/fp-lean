ffecf64877f3810f078101a862c651ebf60984e2
David Thrane Christiansen <david@davidchristiansen.dk>
2025-05-29 03:53:15 +0200

    Incorporate some of the theme from the manual

--- book/Main.lean
+++ book/Main.lean
@@ -10,6 +10,14 @@ def config : Config where
   emitHtmlMulti := true
   htmlDepth := 2
   extraFiles := [("static", "static")]
-  extraCss := ["/static/theme.css"]
-
+  extraCss := [
+    "/static/theme.css",
+    "/static/fonts/source-serif/source-serif-text.css",
+    "/static/fonts/source-code-pro/source-code-pro.css",
+    "/static/fonts/source-sans/source-sans-3.css",
+    "/static/fonts/noto-sans-mono/noto-sans-mono.css"
+  ]
+  logo := some "/static/lean_logo.svg"
+  sourceLink := some "https://github.com/leanprover/fp-lean"
+  issueLink := some "https://github.com/leanprover/fp-lean/issues"
 def main := manualMain (%doc FPLean) (config := config.addKaTeX)
